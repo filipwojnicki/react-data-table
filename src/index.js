8445207baa31b7803911853208107979906feb58
Filip Wojnicki <filip.wojnicki@op.pl>
2020-03-09 03:33:38 +0100

    main component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
+
+import Api from './components/Api/ApiComponent';
+import Table from './components/Table/Table';
+
 import './main.css';
-import App from './components/main/App';
 import * as serviceWorker from './serviceWorker';
+
+export default function App() {
+  const [companies, setCompanies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const tableRef = useRef();
+
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="App">
+      <Api setIsLoading={setIsLoading} setCompanies={setCompanies} />
+      {isLoading ? (<h1>Loading...</h1>) : (
+        <div>
+          <input name="search" type="text" value={value} onChange={e => {setValue(e.target.value); tableRef.current.filterCompanies(e);}} placeholder="Filter the result" />
+          {companies.hasOwnProperty('timestamp') ? <span>Last time data updated: {new Date(companies.timestamp).toUTCString()}</span> : ''}
+          {companies.hasOwnProperty('value') ? <Table companies={companies.value} ref={tableRef} /> : 'No data'}
+        </div>
+      )}
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
