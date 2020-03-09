4cc8589b1c1d4d442bc661a9fc96214b944d90c7
Filip Wojnicki <filip.wojnicki@op.pl>
2020-03-09 03:31:21 +0100

    implemanting api with api components

---
+++ src/class/Api.js
@@ -0,0 +1,63 @@
+
+
+export default class Api {
+  constructor() {
+    this.apiUrl = 'https://recruitment.hal.skygate.io';
+  }
+
+  /**
+   * Get companies list from api. Check session storage for data, thats not longer than 10 minutes.
+   */
+  async getCompanies() {
+    try{
+      let response = await fetch(`${this.apiUrl}/companies`);
+      let companies = await response.json();
+
+      if (!companies.length) {
+        return [];
+      }
+
+      return companies;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  }
+
+  /**
+   * Get income of current company by id.
+   *
+   * @param companyId
+   */
+  async getCurrentCompanyIncomes(companyId) {
+    try {
+      if(!companyId) {
+        return {
+          id: companyId,
+          incomes: []
+        };
+      }
+
+      let response = await fetch(`${this.apiUrl}/incomes/${parseInt(companyId, 10)}`);
+      let incomes = await response.json();
+
+      if (incomes.hasOwnProperty('id') && incomes.hasOwnProperty('incomes')) {
+        if (incomes.incomes.length) {
+          return incomes;
+        }
+      }
+
+      return {
+        id: companyId,
+        incomes: []
+      };
+
+    } catch (e) {
+      console.error(e);
+      return {
+        id: companyId,
+        incomes: []
+      };
+    }
+  }
+}
---
+++ src/components/Api/ApiComponent.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import Api from '../../class/Api';
+
+export default function ApiComponent(params) {
+  const api = new Api();
+
+  useEffect(() => {
+    const getCompanies = async () => {
+      let tempCompanies = sessionStorage.getItem('companies');
+      if (tempCompanies) {
+        tempCompanies = JSON.parse(tempCompanies);
+
+        if (tempCompanies.hasOwnProperty('value') && tempCompanies.hasOwnProperty('timestamp')) {
+          const now = new Date().getTime();
+          const minutesDiff = (now - tempCompanies.timestamp) / 1000 / 60;
+
+          if (tempCompanies.value.length && minutesDiff < 10) {
+            params.setCompanies(tempCompanies);
+            params.setIsLoading(false);
+            return;
+          }
+        }
+      }
+
+      const companies = await api.getCompanies();
+      await updateCompaniesIncomes(companies);
+    };
+
+    const updateCompaniesIncomes = async (companies) => {
+      if (!companies.length) {
+        params.setCompanies({value: [], timestamp: new Date().getTime()});
+        params.setIsLoading(false);
+        return;
+      }
+
+      let companyPromises = [];
+
+      for(let company of companies) {
+        companyPromises.push(api.getCurrentCompanyIncomes(company.id))
+      }
+
+      let responses = await Promise.all(companyPromises).catch(error => console.error(error));
+
+      const prevMonthFirstDay = new Date(new Date().setDate(0)).setDate(1);
+      const prevMonthLastDay = new Date().setDate(0);
+
+      companies = companies.map(company => {
+        for (let companyResponse of responses) {
+          if (company.id === companyResponse.id) {
+            Object.assign(company, companyResponse);
+          }
+        }
+
+        if(company.incomes.length) {
+          let total = company.incomes.reduce((a, b) => a + (Number(b.value) || 0), 0);
+
+          company.total = total;
+          company.avg = total / company.incomes.length;
+          company.lastmonth = company.incomes.filter(inc => {
+            let date = new Date(inc.date);
+            if (date >= prevMonthFirstDay && date <= prevMonthLastDay) {
+              return inc;
+            }
+          }).reduce((a, b) => a + (Number(b.value) || 0), 0);
+        } else {
+          company.total = 0;
+          company.avg = 0;
+          company.lastmonth = 0;
+        }
+
+        return company;
+      });
+
+      let storageData = {
+        value: companies,
+        timestamp: new Date().getTime()
+      };
+      sessionStorage.setItem('companies', JSON.stringify(storageData));
+
+      params.setCompanies(storageData);
+      params.setIsLoading(false);
+    };
+
+    getCompanies();
+  }, []);
+
+  return(null);
+
+}
